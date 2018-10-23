da74cb13c13d00c28676ddb83ecf4c7297f11f9e
m4573r5nip3rj <mail2gahlot@gmail.com>
2018-10-23 13:23:37 +0300

    file added

---
+++ mail.php
@@ -0,0 +1,20 @@
+<?php
+$email_to = "mail2gahlot@gmail.com";
+$name = $_POST["fname"];
+$email_from = $_POST["email"];
+$message = $_POST["message"];
+$email_subject = $_POST["subject"];
+$headers = "From: " . $email_from . "\n";
+$headers .= "Reply-To: " . $email_from . "\n";
+
+$message = "Name: ". $name . "\r\nMessage: " . $message;
+
+ini_set("sendmail_from", $email_from);
+$sent = mail($email_to, $email_subject, $message, $headers, "-f" .$email_from);
+if ($sent)
+{
+header("Location: http://aristocratenterprises.com/");
+} else {
+echo "There has been an error sending your comments. Please try later.";
+}
+php>
